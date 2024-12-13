85c6ed675f8df60f8d277a00d37a1c9841fe9cb6
DuongPhu03 <Duongngocphu2003@gmail.com>
2024-12-13 11:10:09 +0700

    Update Dashboard

--- PetCare/Controllers/AccountController.cs
+++ PetCare/Controllers/AccountController.cs
@@ -196,6 +196,64 @@ namespace PetCare.Controllers
 
             return View(donhang); // Pass the filtered orders to the view
         }
+        public async Task<ActionResult> Details(int id)
+        {
+            int? userId = HttpContext.Session.GetInt32("Username");
+
+            if (userId == null)
+            {
+                HttpContext.Session.Clear();
+                return RedirectToAction("Login", "Account"); // Redirect to login if user is not authenticated
+            }
+
+            var user = await context.Khachhangs.FindAsync(userId);
+            ViewData["UserId"] = user.id_kh;
+            ViewData["UserName"] = user.ten_kh;
+
+            var donhang = await context.Donhangs.FirstOrDefaultAsync(dh => dh.id_dh == id);
+            if (donhang == null) return NotFound("Không tìm thấy đơn hàng");
+
+            var khachhang = await context.Khachhangs.FirstOrDefaultAsync(kh => kh.id_kh == donhang.id_kh);
+            var nhanVien = await context.Nhanviens.FirstOrDefaultAsync(nv => nv.id_nv == donhang.id_nv);
+
+            var chitietdon = await context.Chitietdons.Where(ct => ct.id_dh == id)
+                .Join(context.Sanphams, ct => ct.id_sp, sp => sp.id_sanpham, (ct, sp) => new ChiTietDonHang
+                {
+                    id_sp = ct.id_sp,
+                    ten_sp = sp.ten_sanpham,
+                    so_luong = ct.soluong,
+                    gia_sp = sp.thanhtien,
+                    thanh_tien = ct.soluong * sp.thanhtien,
+                    hinh_anh = sp.hinhanh
+                }).ToListAsync();
+
+            string nhanvien;
+            if (user != null)
+            {
+                nhanvien = nhanVien.ten_nv;
+            }
+            else
+            {
+                nhanvien = "";
+            }
+
+            var viewModel = new VMChitietdh
+            {
+                id_dh = donhang.id_dh,
+                ten_nv = nhanvien,
+                ten_kh = khachhang.ten_kh,
+                sdt_kh = khachhang.sdt_kh,
+                diachi_giao = donhang.diachi_giao,
+                trang_thai = donhang.trang_thai,
+                tong_tien = donhang.tong_tien,
+                ngay_tao = donhang.CreateAt,
+                ghi_chu = donhang.ghi_chu,
+                ma_dh = donhang.ma_dh,
+                giohang = chitietdon
+            };
+
+            return View(viewModel);
+        }
 
         [HttpPost]
         public async Task<IActionResult> Cancel(int id)
